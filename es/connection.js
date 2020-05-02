82ad530c2a3331eba15ea19ca8d5f0ec4f3efd1d
Justin Zint <justin@Justins-MBP-2.lan1>
2020-05-01 22:26:31 -0500

    fix: configure es client for hosted instance #5

--- es/connection.js
+++ es/connection.js
@@ -3,8 +3,8 @@ const { Client } = require('@elastic/elasticsearch')
 
 const IN_PRODUCTION = process.env.NODE_ENV
 const ES_CLOUD_ID = process.env.ES_CLOUD_ID
-const USERNAME = process.env.ES_USERNAME
-const PASSWORD = process.env.ES_PASSWORD
+const ES_USERNAME = process.env.ES_USERNAME
+const ES_PASSWORD = process.env.ES_PASSWORD
 const config = IN_PRODUCTION
     ? {
         cloud: { id: ES_CLOUD_ID },
