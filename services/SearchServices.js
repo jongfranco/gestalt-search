86ce1f1279de421c7b2f066016334a94eb649187
Justin Zint <justin@Justins-MBP-2.lan1>
2020-05-06 20:42:02 -0500

    feat: scoring per result; prep for ensemble #3 #6

--- services/SearchServices.js
+++ services/SearchServices.js
@@ -2,6 +2,41 @@ const es = require('../es/connection')
 const stopwords = require('nltk-stopwords')
 const english = [...stopwords.load('english'), 'what', 'know']
 
+
+const baseParams = {
+    index: 'covid', explain: true, body: { query: {} }
+}
+
+const intervalSearch = (params) => {
+
+}
+
+const multiMatchSearch = (params) => {
+    let base = baseParams
+    base.body.query = {
+        multi_match: {
+            query: params.q,
+            analyzer: 'standard',
+            type: 'best_fields',
+            fuzziness: 'AUTO',
+            fields: ['abstract', 'text'],
+            operator: 'and',
+            minimum_should_match: '90%'
+        }
+    }
+    return es.search({
+        ...base,
+        size: params.size
+    })
+}
+
+const combine = (groups) => {
+    return groups
+}
+
+
+
+
 module.exports = {
     async search(params) {
         const keywords = params.keywords
@@ -11,22 +46,18 @@ module.exports = {
             .split(' ')
             .filter(word => !english.includes(word))
             .join(' ')
-        
+
+        //other ways to include kws?
         if (keywords) q = q + ' ' + keywords.join(' ')
-            
-        const result = es.search({
-            index: 'covid',
-            size: size,
-            body: {
-                query: {
-                    multi_match: {
-                        query: q,
-                        fields: ['abstract', 'text']
-                    },
-                }
-            }
-        })
-        return result
+
+        params = {
+            q: q,
+            size: size
+        }
+
+        const multiMatchResults = multiMatchSearch(params)
+        // const intervalResults = intervalSearch(params)
+
+        return multiMatchResults
     }
 }
-
